a3b4eb511d05f7195a73eb723b0c9b97c5340892
Farrelsp <frozzgaming236@gmail.com>
2025-10-08 07:42:43 +0700

    navbar

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,11 @@
+
+    <nav class="bg-[#3C2E22] dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <span class="self-center text-3xl font-semibold whitespace-nowrap text-white">Sweet Delight</span>
+    </a>
+    <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <button type="button" class="text-white bg-[#847568] hover:bg-[#b8a492] focus:ring-4 focus:outline-none font-medium rounded-lg text-lg px-4 py-2 text-center">+ Tulis Resep</button>
+    </div>
+    </div>
+    </nav>
